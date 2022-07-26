18de909ad5bf2319f3040519f19d63079d45a4d2
Barash777 <barash.vertihvost@gmail.com>
2022-07-26 23:41:37 +0300

    lesson 2 done (commented one function)

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -4,7 +4,7 @@ console.log('lesson 2');
 // http://jsflow.org/docs/lex-env/
 
 //// Closure
-// https://learn.javascript.ru/closure
+// +++ https://learn.javascript.ru/closure
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Closures
 // https://medium.com/@stasonmars/%D0%BF%D0%BE%D0%BD%D0%B8%D0%BC%D0%B0%D0%B5%D0%BC-%D0%B7%D0%B0%D0%BC%D1%8B%D0%BA%D0%B0%D0%BD%D0%B8%D1%8F-%D0%B2-javascript-%D1%80%D0%B0%D0%B7-%D0%B8-%D0%BD%D0%B0%D0%B2%D1%81%D0%B5%D0%B3%D0%B4%D0%B0-c211805b6898
 // +++ https://www.youtube.com/watch?v=pahO5XjnfLA
